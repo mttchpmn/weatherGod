5540788dbe73ddd17f948482f31b6036899df7af
Matt Chapman <matt.j.r.chapman@gmail.com>
2017-05-24 13:07:29 +1200

    Updated to reflect new folder name.

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@
 
 from flask import Flask, jsonify
 
-from weatherGodapp import app
+from app import app
 from lib.rainRadar import RainRadar
 from lib.webcam import NzskiWebcam, AirportWebcam, StaticWebcam
 from lib.snowReports import NzskiReport
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from weatherGodapp import app
+from app import app
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
